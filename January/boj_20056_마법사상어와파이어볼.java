ee09c6714070e82a46af3f13d69dee701df47c93
jaeriiimy <riim715@naver.com>
2021-01-19 17:19:52 +0900

    Study on 210119(cant solve)

--- January/boj_20056_마법사상어와파이어볼.java
+++ January/boj_20056_마법사상어와파이어볼.java
@@ -1,14 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class boj_20056_마법사상어와파이어볼 {
-	static int[] dy = { -1, -1, 0, 1, 1, 1, 0, -1 };
-	static int[] dx = { 0, 1, 1, 1, 0, -1, -1, -1 };
+	static int[] dy = { 0, 1, 1, 1, 0, -1, -1, -1 };
+	static int[] dx = { -1, -1, 0, 1, 1, 1, 0, -1 };
+	static LinkedList<Ball>[][] map;
+	static LinkedList<Ball>[][] next;
+	// 0:북 , 1:북동, 2:동, 3:동남, 4:남, 5:남서, 6:서, 7:북서
 	static int N, M, K;
-	static ArrayList<Ball>[][] map;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,114 +19,82 @@ public class boj_20056_마법사상어와파이어볼 {
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 
-		map = new ArrayList[N][N];
-		for (int i = 0; i < map.length; i++) {
-			for (int j = 0; j < map[i].length; j++) {
-				map[i][j] = new ArrayList<Ball>();
+		map = new LinkedList[N][N];
+		next = new LinkedList[N][N];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				map[i][j] = new LinkedList<Ball>();
+				next[i][j] = new LinkedList<Ball>();
 			}
 		}
+
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int r = Integer.parseInt(st.nextToken()) - 1;
 			int c = Integer.parseInt(st.nextToken()) - 1;
-			int m = Integer.parseInt(st.nextToken());
-			int s = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
 			int d = Integer.parseInt(st.nextToken());
-
-			map[r][c].add(new Ball(m, s, d));
+			map[r][c].add(new Ball(w, v, d));
 		}
 
-		while (K != 0) {
+		for (int i = 0; i < K; i++) {
+			System.err.println(i);
+			print(map);
 			move();
-			K--;
+			cal();
+			System.out.println("===================");
+			print(map);
 		}
 
 		int result = 0;
-		for (int i = 0; i < map.length; i++) {
+		for (int i = 0; i < map.length; i++) {S
 			for (int j = 0; j < map[i].length; j++) {
 				for (int k = 0; k < map[i][j].size(); k++) {
-					result += map[i][j].get(k).m;
+					result += map[i][j].get(k).w;
 				}
 			}
 		}
 		System.out.println(result);
-
 	}
 
 	public static void move() {
-		ArrayList<Ball>[][] newMap = new ArrayList[N][N];
-		for (int i = 0; i < newMap.length; i++) {
-			for (int j = 0; j < newMap[i].length; j++) {
-				newMap[i][j] = new ArrayList<Ball>();
-			}
-		}
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				for (int k = 0; k < map[i][j].size(); k++) {
-					int size = map[i][j].get(k).m;
-					int vel = map[i][j].get(k).s;
-					int dir = map[i][j].get(k).d;
+				if (map[i][j].size() != 0) {
+					Ball b = map[i][j].get(0);
+					map[i][j].remove();
+					int nx = i + dx[b.d] * (b.v % N);
+					int ny = j + dy[b.d] * (b.v % N);
+					nx = checkBoundary(nx);
+					ny = checkBoundary(ny);
 
-					int ny = i + (dy[dir] * vel);
-					int nx = j + (dx[dir] * vel);
-
-					if (N % 2 == 0) {
-						if (ny < 0 && dy[dir] != 0) {
-							ny = Math.abs(ny + N);
-						} else if (ny == N && dy[dir] != 0) {
-							ny = 0;
-						} else if (ny > N && dy[dir] != 0) {
-							ny = (ny + 1) % N;
-						}
-
-						if (nx < 0 && dx[dir] != 0) {
-							nx = Math.abs(nx + N);
-						} else if (nx == N && dx[dir] != 0) {
-							nx = 0;
-						} else if (nx > N && dx[dir] != 0) {
-							nx = (nx + 1) % N;
-						}
-					}
-
-					else {
-						if (ny < 0 && dy[dir] != 0) {
-							ny = Math.abs(ny + N);
-						} else if (ny == N && dy[dir] != 0) {
-							ny = 0;
-						} else if (ny > N && dy[dir] != 0) {
-							ny = (ny + 1) % N - 1;
-						}
-
-						if (nx < 0 && dx[dir] != 0) {
-							nx = Math.abs(nx + N);
-						} else if (nx == N && dx[dir] != 0) {
-							nx = 0;
-						} else if (nx > N && dx[dir] != 0) {
-							nx = (nx + 1) % N - 1;
-						}
-					}
-
-					newMap[ny][nx].add(new Ball(map[i][j].get(k).m, map[i][j].get(k).s, map[i][j].get(k).d));
-
+					next[ny][nx].add(new Ball(b.w, b.v, b.d));
 				}
-				map[i][j].clear();
 			}
 		}
-		calculate(newMap);
 	}
 
-	public static void calculate(ArrayList<Ball>[][] lst) {
+	private static int checkBoundary(int i) {
+		if (i < 0)
+			i += N;
+		if (i >= N)
+			i -= N;
+		return i;
+	}
+
+	public static void cal() {
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				if (lst[i][j].size() >= 2) {
+				if (next[i][j].size() >= 2) {
 					int totSize = 0;
 					int totVel = 0;
 					int totDir = 0;
-					int tmpSize = lst[i][j].size();
-					for (int k = 0; k < lst[i][j].size(); k++) {
-						totSize += lst[i][j].get(k).m;
-						totVel += lst[i][j].get(k).s;
-						totDir += lst[i][j].get(k).d;
+					int tmpSize = next[i][j].size();
+					for (int k = 0; k < next[i][j].size(); k++) {
+						totSize += next[i][j].get(k).w;
+						totVel += next[i][j].get(k).v;
+						totDir += next[i][j].get(k).d;
 					}
 					if (totSize / 5 != 0) {
 						for (int k = 0; k < 4; k++) {
@@ -135,41 +105,40 @@ public class boj_20056_마법사상어와파이어볼 {
 							}
 						}
 					}
-				} else if (lst[i][j].size() == 1) {
-					map[i][j].add(lst[i][j].get(0));
+				} else if (next[i][j].size() == 1) {
+					map[i][j].add(next[i][j].get(0));
 				}
-				lst[i][j].clear();
+				next[i][j].clear();
 			}
 		}
-//		print(map)
 	}
 
 	static class Ball {
-		int m;
-		int s;
+		int w;
+		int v;
 		int d;
 
-		public Ball(int m, int s, int d) {
-			this.m = m;
-			this.s = s;
+		public Ball(int w, int v, int d) {
+			this.w = w;
+			this.v = v;
 			this.d = d;
 		}
 	}
 
-	public static void print(ArrayList<Ball>[][] m) {
-		for (int i = 0; i < m.length; i++) {
-			for (int j = 0; j < m[i].length; j++) {
-				for (int k = 0; k < m[i][j].size(); k++) {
-					System.out.print(m[i][j].get(k).m + " " + m[i][j].get(k).s + " " + m[i][j].get(k).d + ", ");
-
-				}
-				System.out.print(" ");
-				if (m[i][j].size() == 0) {
-					System.out.print("^^^^^ ");
+	public static void print(LinkedList<Ball>[][] map) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (map[i][j].size() == 0) {
+					System.out.print("  X   ");
+				} else {
+					for (int k = 0; k < map[i][j].size(); k++) {
+						System.out.print(map[i][j].get(k).v + ", " + map[i][j].get(k).d + " ");
+					}
 				}
 			}
 			System.out.println();
+
 		}
-		System.out.println();
 	}
+
 }
