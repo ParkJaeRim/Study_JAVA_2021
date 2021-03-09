65e60e04b2d0175a0c69a04dfb5231c93a037318
ParkJaeRim <riim715@naver.com>
2021-03-09 21:49:12 +0900

    Study on 210309

--- March/BinaryTree.java
+++ March/BinaryTree.java
@@ -13,9 +13,15 @@ public class BinaryTree {
 			add(N);
 		}
 //		preorder(rootNode);
+		inorder(rootNode);
+		System.out.println(rootNode.toString());
+		delete(3, rootNode);
+		System.out.println("===결과===");
+		System.out.println(rootNode.toString());
 //		inorder(rootNode);
 //		postorder(rootNode);
-		find(52, 1, rootNode);
+//		find(52, 1, rootNode);
+
 	}
 
 	public static void add(int N) {
@@ -41,6 +47,29 @@ public class BinaryTree {
 				}
 			}
 		}
+	}
+
+	public static void delete(int N, Node findNode) {
+		System.out.println("============= : " + N + " " + findNode.value);
+		System.out.println(findNode.toString());
+		if (rootNode.value == N) {
+			rootNode = null;
+
+		} else if (findNode.leftChild != null && findNode.leftChild.value == N) {
+			findNode.leftChild = null;
+
+		} else if (findNode.leftChild != null && findNode.rightChild.value == N) {
+			findNode.rightChild = null;
+		} else if (findNode.rightChild.value != N && findNode.rightChild.value != N) {
+			if (findNode.value < N) {
+				System.out.println("#1");
+				delete(N, findNode.rightChild);
+			} else if (findNode.value > N) {
+				System.out.println("#2");
+				delete(N, findNode.leftChild);
+			}
+		}
+
 	}
 
 	public static void find(int finding, int cnt, Node now) {
@@ -88,6 +117,11 @@ public class BinaryTree {
 			this.leftChild = null;
 			this.rightChild = null;
 
+		}
+
+		@Override
+		public String toString() {
+			return "Node [value=" + value + ", leftChild=" + leftChild + ", rightChild=" + rightChild + "]";
 		}
 	}
 }
